a32335a862f7e38213d14360accbe2689d357dba
Colin Moore <colin.moore@rogers.com>
2022-01-07 14:49:41 -0500

    Removed frameSortFunc

--- AlgorithmTraining/Trainer.cpp
+++ AlgorithmTraining/Trainer.cpp
@@ -355,32 +355,3 @@ float Trainer::calculateEntropyFrame(cv::Mat& ImageFrame){
     return ImgEntropy;
 }
 
-
-/*Misc functions*/
-
-/*! \brief Sorting function for camera frames
- *
- * To be used by std::sort for sorting files associated with
- * the camera frames. Not to be used otherwise.
- */
-
-bool Trainer::frameSortFuncTrainer(std::string i, std::string j)
-{
-
-    unsigned int sequence_i, camera_i;
-    int got_i = sscanf(i.c_str(), this->ImageFormat.c_str(),
-                       &camera_i, &sequence_i
-                      );
-
-
-    assert(got_i == 2);
-
-    unsigned int  sequence_j, camera_j;
-    int got_j = sscanf(j.c_str(),  this->ImageFormat.c_str(),
-                       &camera_j, &sequence_j
-                      );
-    assert(got_j == 2);
-
-    return sequence_i < sequence_j;
-
-}
